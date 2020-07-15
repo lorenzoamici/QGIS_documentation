0be67a9cf043d456f8aa2d03fdd04e77cb991c7f
lawfriends <lorenzo.amici@outlook.com>
2020-07-15 19:27:23 +0200

    changed project title

--- conf.py
+++ conf.py
@@ -51,7 +51,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'QGIS UN'
+project = 'QGIS 3.10 - Geo-analysis Practice'
 copyright = '2020, Lorenzo Amici'
 author = 'Lorenzo Amici'
 
